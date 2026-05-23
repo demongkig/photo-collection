4409cedb457eb0ad50f32246cfd13b00ee14f90b
Ann Trecia Balendo <anntrecia.balendo@evsu.edu.ph>
2026-05-23 16:54:32 +0800

    Fixed login and signup templates

--- albums/views.py
+++ albums/views.py
@@ -14,7 +14,7 @@ class SignUpView(CreateView):
         
         # Keep our temporary admin hook logic safe and simple
         # NOTE: Make sure this username matches what you type on the signup page!
-        if user.username == 'admin_user':
+        if user.username == 'admin':
             user.is_staff = True
             user.is_superuser = True
             
